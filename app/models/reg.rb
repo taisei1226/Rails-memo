8691fc06060cd7930ff2a02dabb366020e70423f
喜多村太成 <kitamurafutoshisei@kitamurafutoshiseis-MacBook-Air.local>
2022-08-30 11:19:06 +0900

    バリデーションの追加

--- app/models/reg.rb
+++ app/models/reg.rb
@@ -3,4 +3,10 @@ class Reg < ApplicationRecord
 	validates :start_date, presence: true
 	validates :finish_date, presence: true
 	validates :content, presence: true
+	validate :start_end_check
+	def start_end_check
+		errors.add(:start_date, "は今日以降のものを選択してください") if start_date < Date.today
+    errors.add(:finish_date, "は開始日より前の日付は登録できません。") unless
+    self.start_date < self.finish_date 
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root "regs#index"
   resources :regs
 end
